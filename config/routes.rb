2c7bffd1c969618b802249a5ec09ff65914bf231
J K <github@jonahkallen.com>
2021-06-02 19:37:31 -0700

    test changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ require_dependency "discourse_basic_editor_constraint"
 
 DiscourseBasicEditor::Engine.routes.draw do
   get "/" => "discourse_basic_editor#index", constraints: DiscourseBasicEditorConstraint.new
-  get "/actions" => "actions#index", constraints: DiscourseBasicEditorConstraint.new
-  get "/actions/:id" => "actions#show", constraints: DiscourseBasicEditorConstraint.new
 end
